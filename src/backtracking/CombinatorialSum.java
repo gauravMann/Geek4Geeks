7ee2a0bf39e3803c259ec7d3c8363495d27cefd9
mann <mann@HAL9000.localdomain>
2018-07-18 15:35:41 +0530

    Done

--- src/backtracking/CombinatorialSum.java
+++ src/backtracking/CombinatorialSum.java
@@ -2,68 +2,64 @@ package backtracking;
 
 //import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class CombinatorialSum {
 
-	public static void printPath(int[] coins, int src, int sum, StringBuilder sb, int called) {
-		if (src == sum) {
-			System.out.print("(");
-			for (int i = 0; i < sb.length(); i++) {
-				System.out.print(sb.charAt(i) + " ");
-			}
-			System.out.print(")");
-			return;
-		}
-
-		for (int denom = called; denom < coins.length; denom++) {
-			int inter = src + coins[denom];
-			if (inter <= sum) {
-				sb.append(coins[denom]);
-
-				printPath(coins, inter, sum, sb, denom);
-				sb.deleteCharAt(sb.length() - 1);
-
-			}
-		}
-
-	}
-
-	public static void solve(int[] coins, int vidx, StringBuilder sb, int sum) {
-		if(sum < 0){
+	public static void solve(int[] coins, int vidx, StringBuilder sb,
+							 String ssf, int targetLeft) {
+		
+		if (targetLeft < 0) {
 			return;
 		}
 		if (vidx == coins.length) {
-			if (sum == 0) {
-				System.out.print("(" + sb + ")");
+			if (targetLeft == 0) {
+				sb.append(("(" + ssf.trim() + ")"));
 			}
 			return;
 		}
-		
-		solve(coins, vidx, sb.append(coins[vidx]), sum - coins[vidx]);
-		sb.deleteCharAt(sb.length() - 1);
-		solve(coins, vidx + 1, sb, sum);
+
+
+		solve(coins, vidx, sb, ssf + coins[vidx] + " ", targetLeft - coins[vidx]);
+
+
+		solve(coins, vidx + 1, sb, ssf, targetLeft);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		int testCase = scn.nextInt();
 
 		while (testCase-- > 0) {
 			int n = scn.nextInt();
-			int[] coins = new int[n];
-
+			
+			HashSet<Integer> set = new HashSet<>();
 			for (int i = 0; i < n; i++) {
-				coins[i] = scn.nextInt();
+				set.add(scn.nextInt());
 			}
-			Arrays.sort(coins);
 			int sum = scn.nextInt();
-			scn.close();
-
-			solve(coins, 0, new StringBuilder(""),  sum);
-			//printPath(coins, 0, sum, new StringBuilder(""), 0);
-			System.out.println();
+			
+			int[] coins = new int[set.size()];
+			Iterator<Integer> it  = set.iterator();
+			int i = 0;
+			while(it.hasNext()) {
+				coins[i] = it.next();
+				it.remove();
+				i++;
+			}
+			
+			Arrays.sort(coins);
+			StringBuilder sb = new StringBuilder();
+			
+			solve(coins, 0,sb,"", sum);
+			
+			if(sb.length() == 0) {
+				System.out.println("Empty");
+			}else {
+				System.out.println(sb);
+			}
 		}
 
 	}
